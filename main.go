cf0ddc43a9f4043563e6b9c3837b2af49a148fcc
Francesco Mari <francesco.mari@vier.ai>
2024-06-10 10:29:44 +0200

    set up server connection

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "form.html", nil)
+	})
+
+	//r.POST("/analyze", func(c *gin.Context) {
+	//	url := c.PostForm("url")
+	//	result, err := analyzePage(url)
+	//	if err != nil {
+	//		result.ErrorMessage = err.Error()
+	//	}
+	//	c.HTML(http.StatusOK, "result.html", result)
+	//})
+
+	r.Run(":8080")
 }
